1d8d3454042c38aecb371407aea29d58b12fa932
RenXusheng233 <782449909@qq.com>
2021-01-25 09:40:39 +0800

    chore: 全局样式及axios封装优化.

--- src/api/intercept.ts
+++ src/api/intercept.ts
@@ -11,27 +11,52 @@ const request = axios.create({
   // API 请求的默认前缀
   baseURL: process.env.VUE_APP_BASE_URL,
   timeout: 10000, // 请求超时时间
-});
+})
 
 // 异常拦截处理器
 const errorHandler = (error: any) => {
-  const status = get(error, 'response.status');
+  const status = get(error, 'response.status')
   switch (status) {
-    case 400: error.message = '请求错误'; break;
-    case 401: error.message = '未授权，请登录'; break;
-    case 403: error.message = '拒绝访问'; break;
-    case 404: error.message = `请求地址出错: ${error.response.config.url}`; break;
-    case 408: error.message = '请求超时'; break;
-    case 500: error.message = '服务器内部错误'; break;
-    case 501: error.message = '服务未实现'; break;
-    case 502: error.message = '网关错误'; break;
-    case 503: error.message = '服务不可用'; break;
-    case 504: error.message = '网关超时'; break;
-    case 505: error.message = 'HTTP版本不受支持'; break;
-    default: break;
+    case 400:
+      error.message = '请求错误'
+      break
+    case 401:
+      error.message = '未授权，请登录'
+      window.location.href = '/login' // FIXME: router.push
+      storage().remove('ACCESS_TOKEN')
+      break
+    case 403:
+      error.message = '拒绝访问'
+      break
+    case 404:
+      error.message = `请求地址出错: ${error.response.config.url}`
+      break
+    case 408:
+      error.message = '请求超时'
+      break
+    case 500:
+      error.message = '服务器内部错误'
+      break
+    case 501:
+      error.message = '服务未实现'
+      break
+    case 502:
+      error.message = '网关错误'
+      break
+    case 503:
+      error.message = '服务不可用'
+      break
+    case 504:
+      error.message = '网关超时'
+      break
+    case 505:
+      error.message = 'HTTP版本不受支持'
+      break
+    default:
+      break
   }
-  return Promise.reject(error);
-};
+  return Promise.reject(error)
+}
 
 // request interceptor
 request.interceptors.request.use((config: AxiosRequestConfig) => {
