17a09de37fb35b7d7208174ad670effcc5361b9a
RenXusheng233 <782449909@qq.com>
2021-02-04 17:52:49 +0800

    chore: study - base layout.

--- src/views/study/index.vue
+++ src/views/study/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="study">study</div>
+  <div class="study">
+    <div class="decoration"></div>
+    <div class="content">
+      <ul class="content-item" v-for="i in count" :key="i">
+        <li>{{ i }}</li>
+        <el-divider content-position="center">我是分割线</el-divider>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'Study',
+  setup() {
+    const count = ref<number>(50)
+
+    return {
+      count,
+    }
+  },
 })
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+@import './index.less';
+</style>
