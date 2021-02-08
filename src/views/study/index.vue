134d7db83bac6d576fc46d41e48969726dda0b79
RenXusheng233 <782449909@qq.com>
2021-02-08 11:37:40 +0800

    chore: update.

--- src/views/study/index.vue
+++ src/views/study/index.vue
@@ -5,14 +5,16 @@
         <div class="list">list</div>
         <div class="label">label</div>
       </div>
-      <div class="decoration-chart">apexchart</div>
+      <div class="decoration-calendar">
+        <el-calendar v-model="calendarVal"></el-calendar>
+      </div>
     </div>
     <div class="content">
       <ul class="content-item" v-for="i in count" :key="i">
         <li>{{ i }}</li>
         <el-divider content-position="center">我是分割线</el-divider>
       </ul>
-      <el-backtop target=".content" style="right: 20px; bottom: 36px"></el-backtop>
+      <el-backtop target=".content" style="right: 70px; bottom: 25px"></el-backtop>
     </div>
   </div>
 </template>
@@ -22,9 +24,12 @@ import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'Study',
   setup() {
+    const calendarVal = ref(new Date())
+
     const count = ref<number>(50)
 
     return {
+      calendarVal,
       count,
     }
   },
